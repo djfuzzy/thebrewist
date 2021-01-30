e9d86ec5e75ed3eba71111baeac1aebbdee685f9
Dillon Forsberg <djforsberg@hotmail.com>
2021-01-30 12:37:08 -0600

    Add Punk API request caching

--- app/controllers/beer.controller.js
+++ app/controllers/beer.controller.js
@@ -1,29 +1,36 @@
 const axios = require('axios');
+const cache = require('memory-cache');
+
 const beerRatingsDb = require('../utilities/db-helper').loadBeerRatingsDb;
 
 exports.findByName = (req, res) => {
   const name = req.params.name.replace(' ', '_');
   const punkApiRequestUrl = `https://api.punkapi.com/v2/beers?beer_name=${name}`;
 
-  axios
-    .get(punkApiRequestUrl)
-    .then((response) => {
-      console.log(response);
-      res.send(
-        response.data?.map((item) => ({
+  const cachedQuery = cache.get(name);
+
+  if (cachedQuery) {
+    res.send(cachedQuery);
+  } else {
+    axios
+      .get(punkApiRequestUrl)
+      .then((punkApiRes) => {
+        const beers = punkApiRes.data?.map((item) => ({
           id: item.id,
           name: item.name,
           description: item.description,
           first_brewed: item.first_brewed,
           food_pairing: item.food_pairing,
-        }))
-      );
-    })
-    .catch((error) => {
-      res
-        .status(500)
-        .send(`There was a problem retrieving the list of beer: ${error}`);
-    });
+        }));
+        cache.put(name, beers);
+        res.send(beers);
+      })
+      .catch((error) => {
+        res
+          .status(500)
+          .send(`There was a problem retrieving the list of beers: ${error}`);
+      });
+  }
 };
 
 // TODO: This doesn't work
