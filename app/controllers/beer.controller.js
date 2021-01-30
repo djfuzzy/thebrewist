5f06c5ae5f89b1898a8409e02d1dbbb3dab44a94
Dillon Forsberg <djforsberg@hotmail.com>
2021-01-29 22:49:41 -0600

    Replace spaces with underscores

--- app/controllers/beer.controller.js
+++ app/controllers/beer.controller.js
@@ -3,10 +3,11 @@ const noSql = require('nosql');
 const beerRatingsDb = noSql.load('./data/beer-ratings.nosql');
 
 exports.findByName = (req, res) => {
-  const name = req.params.name;
-  // TODO: Replace spaces with underscores.
+  const name = req.params.name.replace(' ', '_');
+  const punkApiRequestUrl = `https://api.punkapi.com/v2/beers?beer_name=${name}`;
+
   axios
-    .get(`https://api.punkapi.com/v2/beers?beer_name=${name}`)
+    .get(punkApiRequestUrl)
     .then((response) => {
       console.log(response);
       res.send(
@@ -30,6 +31,7 @@ exports.findByName = (req, res) => {
 const findBeerRatingById = (id) =>
   beerRatingsDb.find().make((builder) => {
     builder.where('id', id);
+
     return builder.callback((err, response) => {
       if (err) {
         console.log(`An error occurred: ${err}`);
